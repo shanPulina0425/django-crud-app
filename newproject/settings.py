d05047898617b8041fa4838774a0b3a85b7854e7
Shan Pulina <shangtav@gmail.com>
2026-08-29 21:35:41 +0530

    Update database configuration to use MySQL instead of SQLite

--- newproject/settings.py
+++ newproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'django_crud',
+        'USER':'root',
+        'PASSWORD':'81805500',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
